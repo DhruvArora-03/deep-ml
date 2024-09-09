efc3eec67a7cbff4cdbb0924360250f403ae1360
Dhruv Arora <dhruvarora.us@gmail.com>
2024-09-08 22:54:10 -0500

    rref

--- 48-row-reduced-echelon-form.py
+++ 48-row-reduced-echelon-form.py
@@ -1,29 +1,38 @@
 import numpy as np
 import numpy.typing as npt
 
-def rref(matrix: npt.ArrayLike) -> npt.ArrayLike:
-    res = np.array([])
 
-    
+def rref(A: npt.ArrayLike) -> npt.ArrayLike:
+    # pylint: disable-next=consider-using-enumerate
+    for i in range(len(A)):
+        if A[i, i] == 0:
+            nonzero_rel_locs = A[i:, i].nonzero()[0]
+            if len(nonzero_rel_locs) == 0:
+                continue
+            A[i] += A[nonzero_rel_locs[0] + i]
 
-    return res
+        A[i] = A[i] / A[i, i]
+        # pylint: disable-next=consider-using-enumerate
+        for j in range(len(A)):
+            if i == j:
+                continue
 
-if __name__ == '__main__':
-    mat = np.array([
-        [1, 2, -1, -4],
-        [2, 3, -1, -11],
-        [-2, 0, -3, 22]
-    ])
+            A[j] -= A[j, i] * A[i]
+
+    return A
+
+
+if __name__ == "__main__":
+    mat = np.array([[1, 2, -1, -4], [2, 3, -1, -11], [-2, 0, -3, 22]])
 
     rref_matrix = rref(mat)
-
     expected = np.array([
-        [ 1,  0,  0, -8,],
-        [ 0,  1,  0,  1,],
-        [-0, -0,  1, -2,]
+        [ 1, 0, 0, -8, ],
+        [ 0, 1, 0, 1, ],
+        [ -0, -0, 1, -2, ],
     ])
     try:
         np.testing.assert_array_equal(rref_matrix, expected)
-        print('Test passed!')
+        print("Test passed!")
     except AssertionError as e:
-        print('Test failed:', str(e))
+        print("Test failed:", str(e))
